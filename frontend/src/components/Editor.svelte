63fdbcf4cf02d29a8f192636f0be090347d883f7
bartosz121 <bmagiera121@gmail.com>
2022-01-19 21:47:50 +0100

    Add `Editor` component
    - Using `monaco-editor`

---
+++ frontend/src/components/Editor.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { onMount } from "svelte";
+  import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
+
+  export let isReadOnly = false;
+  export let content = "";
+  let editor;
+
+  onMount(() => {
+    monaco.editor.create(editor, {
+      value: content,
+      readOnly: isReadOnly,
+      fontSize: 16,
+      language: "plaintext",
+      minimap: {
+        enabled: false,
+      },
+      automaticLayout: true,
+      padding: {
+        top: 15,
+        bottom: 15,
+      },
+    });
+    monaco.editor.setTheme("vs-dark");
+
+    return () => {
+      editor.dispose();
+    };
+  });
+</script>
+
+<div class="editor h-full" bind:this={editor} />
